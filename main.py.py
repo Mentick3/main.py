2b220c1dab047ae1f434702207ca141672a50a40
Mentick3 <satishsahani234@gmail.com>
2025-06-14 19:40:21 +0530

    Update main.py.py

--- main.py.py
+++ main.py.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import requests
-from bs4 import requirements.txt
+from bs4 requirements.txt
 import yfinance as yf
 import nltk
 from nltk.sentiment import SentimentIntensityAnalyzer
